d7bae4cc3e626df0b425dd20ed846df06185e688
hitem <patrik@boi.se>
2023-04-18 19:38:06 +0200

    New version to remove double entries

--- rss_aggregator.py
+++ rss_aggregator.py
@@ -17,7 +17,6 @@ rss_feed_urls = [
     "https://techcommunity.microsoft.com/gxcuf89792/rss/board?board.id=MicrosoftDefenderCloudBlog",
     "https://techcommunity.microsoft.com/gxcuf89792/rss/board?board.id=MicrosoftDefenderATPBlog",
     "https://techcommunity.microsoft.com/gxcuf89792/rss/board?board.id=MicrosoftDefenderIoTBlog",
-    # Add more RSS feed URLs here
 ]
 
 # Set the output file name
@@ -29,16 +28,25 @@ for url in rss_feed_urls:
     feed = feedparser.parse(url)
     all_entries.extend(feed.entries)
 
-# Remove duplicates based on the 'id' or 'link' field
-unique_entries = {entry.id if hasattr(entry, "id") else entry.link: entry for entry in all_entries}.values()
+# Remove duplicates based on the 'title', 'published', and 'link' fields
+entry_key = lambda entry: (entry.title, entry.published, entry.link)
+unique_entries = {entry_key(entry): entry for entry in all_entries}.values()
+
+# Filter entries published within the last 2 hours
+time_threshold = datetime.datetime.utcnow() - datetime.timedelta(hours=2)
+recent_entries = [entry for entry in unique_entries if datetime.datetime.strptime(entry.published, "%a, %d %b %Y %H:%M:%S %Z") >= time_threshold]
+
+# Filter out entries older than a year
+max_age = datetime.datetime.utcnow() - datetime.timedelta(days=365)
+filtered_entries = [entry for entry in recent_entries if datetime.datetime.strptime(entry.published, "%a, %d %b %Y %H:%M:%S %Z") >= max_age]
 
 # Sort entries by published date in descending order
-sorted_entries = sorted(unique_entries, key=lambda x: x.published_parsed, reverse=True)
+sorted_entries = sorted(filtered_entries, key=lambda x: x.published_parsed, reverse=True)
 
 # Create a new XML tree for the aggregated RSS feed
 root = etree.Element("rss", version="2.0")
 channel = etree.SubElement(root, "channel")
-etree.SubElement(channel, "title").text = "Aggregated Microsoft Blog Feed"
+etree.SubElement(channel, "title").text = "RSS Aggregator Feed"
 etree.SubElement(channel, "link").text = "https://hitem.github.io/rss-aggregator/aggregated_feed.xml"
 etree.SubElement(channel, "description").text = "An aggregated feed of Microsoft blogs"
 etree.SubElement(channel, "lastBuildDate").text = datetime.datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")
